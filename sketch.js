cc26a079f8b3066e77aa2e56b43c8f811b6b73f2
BeyondConfused <114650102+BeyondConfused@users.noreply.github.com>
2022-11-08 14:15:56 +0100

    Added player and game controller

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 var cnv;
+let players = []
+let ball;
+let gameController;
 
 function setup() {
   cnv = createCanvas(750, window.innerHeight);
@@ -6,6 +9,9 @@ function setup() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
+  gameController = new GC()
+  gameController.gameStart()
+
 }
 
 function centerCanvas() {
@@ -20,4 +26,39 @@ function windowResized() {
 
 function draw() {
   background(0);
+
+}
+
+
+function keyPressed(){
+ 
+}
+
+class Player {
+  constructor(name, height, width,x ,y) {
+    this.name = name;
+    this.height = height;
+    this.width = width;
+    this.x = x;
+    this.y = y;
+    console.log("Player",name+" spawned")
+  }
+  move(){
+    if(keyCode==32){
+      console.log("yo")
+    }
+  }
+}
+
+
+class GC {
+  constructor() {
+  }
+  gameStart(){
+    for (let i = 1; i < 3; i++) {
+     players.push(new Player(i,20,30)) 
+    }
+
+
+  }
 }
